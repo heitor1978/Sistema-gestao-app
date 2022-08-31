d08aebee2b3d8be36f009870b5f4ef8cd70ec980
AlexSan1910 <alex.gsan11@gmail.com>
2022-08-30 22:46:09 -0300

    Colocando imagem em evidencia

--- lib/View/Login/login.dart
+++ lib/View/Login/login.dart
@@ -52,59 +52,94 @@ class _LoginState extends State<Login> {
         ),
         preferredSize: const Size.fromHeight(0),
       ),
-      body: Form(
-        key: _formKey,
-        child: Center(
-          child: ListView(
-            children: [
-              Container(
-                padding: const EdgeInsets.fromLTRB(0, 30, 0, 80),
-                /*decoration: const BoxDecoration(
-                  color: primaryColor,
-                  borderRadius: BorderRadius.only(  // FUNÇÃO DE COLOCAR BORDA NA LOGO DA FOTO
-                    bottomLeft: Radius.elliptical(500, 70),
-                    bottomRight: Radius.elliptical(500, 70),
+      body: Stack(children: [
+        Form(
+          key: _formKey,
+          child: Center(
+            child: ListView(
+              children: [
+                Container(
+                  padding: const EdgeInsets.fromLTRB(0, 210, 0, 0),
+                  decoration: const BoxDecoration(
+                    color: primaryColor,
+                    borderRadius: BorderRadius.only(
+                        // FUNÇÃO DE COLOCAR BORDA NA LOGO DA FOTO
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20)),
                   ),
-                ),*/
+                  /*child: Image.asset(
+                    'assets/images/chiquinho-logo.png',
+                    height: 250,
+                  ),*/
+                ),
+                SizedBox(
+                  height:
+                      150, // precisa resolver está questão ////////////////////////////////////////////////
+                ),
+                CustomTextField(
+                  labelText: "Email",
+                  placeholder: "colaborador@email.com",
+                  inputType: TextInputType.emailAddress,
+                  onSaved: (value) => email = value,
+                  validator: (value) => UserValidator.validarEmail(value!),
+                ),
+                CustomTextField(
+                  labelText: "Senha",
+                  placeholder: "********",
+                  obscureText: true,
+                  onSaved: (value) => senha = value,
+                  validator: (value) => UserValidator.validarSenha(value!),
+                ),
+                (loading)
+                    ? const Center(
+                        child: CircularProgressIndicator(
+                          color: primaryColor,
+                        ),
+                      )
+                    : CustomTextButton(
+                        buttonText: "Entrar",
+                        onPressed: () => _login(context),
+                      ),
+                const SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 5,
+                width: 20,
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(7),
+                height: MediaQuery.of(context).size.height / 4,
+                width: MediaQuery.of(context).size.height / 4,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 25.0,
+                      spreadRadius: 0.1,
+                    ),
+                  ],
+                ),
                 child: Image.asset(
                   'assets/images/chiquinho-logo.png',
                   height: 250,
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomTextField(
-                labelText: "Email",
-                placeholder: "colaborador@email.com",
-                inputType: TextInputType.emailAddress,
-                onSaved: (value) => email = value,
-                validator: (value) => UserValidator.validarEmail(value!),
-              ),
-              CustomTextField(
-                labelText: "Senha",
-                placeholder: "********",
-                obscureText: true,
-                onSaved: (value) => senha = value,
-                validator: (value) => UserValidator.validarSenha(value!),
-              ),
-              (loading)
-                  ? const Center(
-                      child: CircularProgressIndicator(
-                        color: primaryColor,
-                      ),
-                    )
-                  : CustomTextButton(
-                      buttonText: "Entrar",
-                      onPressed: () => _login(context),
-                    ),
-              const SizedBox(
-                height: 30,
-              ),
             ],
           ),
-        ),
-      ),
+        )
+      ]),
     );
   }
 }
